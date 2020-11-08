b2b0d15873f72db4f54878935daf205c4f9d8906
Hercules Gabriel <herculesgabriel00@gmail.com>
2020-11-08 10:10:35 -0300

    testing deploy

---
+++ build/precache-manifest.397c9d06c56e9307510c7f6f06acd399.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e23c143239ed8fb557605356a693d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3087f55417381f1809f4",
+    "url": "/static/css/main.c0548c0a.chunk.css"
+  },
+  {
+    "revision": "2ff31ba402b8de13706e",
+    "url": "/static/js/2.5b77b7ad.chunk.js"
+  },
+  {
+    "revision": "8d190e2d02f8e768921a63f56a741895",
+    "url": "/static/js/2.5b77b7ad.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3087f55417381f1809f4",
+    "url": "/static/js/main.9756f28a.chunk.js"
+  },
+  {
+    "revision": "4d2421473a5f9d875040",
+    "url": "/static/js/runtime-main.2cca0beb.js"
+  }
+]);
